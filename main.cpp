f4725afd9f00b872022c82bb4c31e43e26b4d8fb
NahuelArrieta <99760313+NahuelArrieta@users.noreply.github.com>
2025-05-25 20:15:04 -0300

    Parallel (#1)
    * add mpi
    
    * minor fix
    
    * fix

--- main.cpp
+++ main.cpp
@@ -5,10 +5,18 @@
 #include "secuential/quickSort.cpp" 
 #include "utils/fileReader.cpp"
 #include "utils/checkSorted.cpp"
+#include "parallel/quicksort.cpp"
 
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
+    // Initialize MPI
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
     string filePath = "numeros_aleatorios.csv"; // Path to the CSV file
 
     vector<vector<int>> data = readFile(filePath);
@@ -24,13 +32,26 @@ int main() {
         // Start timer 
         auto start = chrono::high_resolution_clock::now();
 
-        quickSort(row);
+
+        parallelQuickSort(row);
+        
    
         // End timer
         auto end = chrono::high_resolution_clock::now();
         double duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
 
         // Check if the row is sorted
+        if (rank != 0) {
+            // Only the master process checks if the row is sorted
+            continue;
+        }
+
+        // Print the sorted row
+        cout << "Sorted row: ";
+        for (const auto& num : row) {
+            cout << num << " ";
+        }
+
         if (!checkSorted(row)) {
             cerr << "Row is not sorted." << endl;
             return 1;
@@ -39,6 +60,9 @@ int main() {
         // Print the duration
         cout << "Row of size " << row.size() << " sorted in " << duration << " ms." << endl;    
     }
+
+    // Finalize MPI
+    MPI_Finalize();
     
     return 0;
 }
---
+++ parallel/quicksort.cpp
@@ -0,0 +1,107 @@
+
+#include <iostream>
+#include <vector>
+#include <mpi.h>
+
+
+using namespace std;
+
+int parallelQuickSort(std::vector<int>& arr) {
+    
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // Master process 
+    vector<int> sorted_array; // Define sorted_array here to make it accessible later
+    if (rank == 0) {
+        // Calculate the sum of all elements
+        int sum = 0;
+        for (int i = 0; i < arr.size(); ++i) {
+            sum += arr[i];
+        }
+
+        // The number of pivots is equal to the number of processes minus one
+        // Get the pivot values to distribute between processes
+        int average_value = sum / arr.size();
+        int pivot_gap = average_value * 2 / size;
+
+        vector<int> pivots(size - 1);
+        int current_pivot =  0;
+        for (int i = 0; i < size - 1; ++i) {
+            pivots[i] = current_pivot;
+            current_pivot += pivot_gap;
+        }
+
+        vector<vector<int>> chunks(size);
+        for (int i = 0; i < arr.size(); ++i) {
+            bool assigned = false;
+            for (int j = 0; j < size - 1; ++j) {
+                // Check if the element is less than the pivot
+                if (arr[i] < pivots[j]) {
+                    chunks[j].push_back(arr[i]);
+                    assigned = true;
+                    break;
+                }
+            }
+
+            // If the element is not assigned to any chunk, add it to the last chunk
+            if (!assigned) {
+                chunks[size - 1].push_back(arr[i]);
+            }
+        }
+
+        // Send the chunks to all processes
+        vector<int> local_chunk;
+        int chunk_size;
+        for (int i = 1; i < size; ++i) {
+            chunk_size = chunks[i].size();
+            MPI_Send(&chunk_size, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            if (chunk_size > 0) {
+                MPI_Send(chunks[i].data(), chunk_size, MPI_INT, i, 0, MPI_COMM_WORLD);
+            }
+        }
+
+        // Sort the local chunk
+        local_chunk = chunks[0]; // Master process sorts its own chunk
+        quickSort(local_chunk);
+        sorted_array = local_chunk; // Initialize sorted_array with the master's local chunk
+
+
+        for (int i = 1; i < size; ++i) {
+            MPI_Recv(&chunk_size, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            if (chunk_size > 0) {
+                local_chunk.resize(chunk_size);
+                MPI_Recv(local_chunk.data(), chunk_size, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                sorted_array.insert(sorted_array.end(), local_chunk.begin(), local_chunk.end());
+            }
+        }
+
+        // Return the sorted array to the master process
+        arr = sorted_array;
+
+    } else {
+        // Other processes receive their chunks
+        int chunk_size;
+        MPI_Recv(&chunk_size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        vector<int> local_chunk(chunk_size);
+        if (chunk_size > 0) {
+            MPI_Recv(local_chunk.data(), chunk_size, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+
+        // Sort the local chunk
+        quickSort(local_chunk);
+
+        // Send the sorted local chunk back to the master process
+        MPI_Send(&chunk_size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        if (chunk_size > 0) {
+            // Send the sorted local chunk back to the master process
+            MPI_Send(local_chunk.data(), chunk_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        }
+    }
+
+    
+
+    return 0;
+}
