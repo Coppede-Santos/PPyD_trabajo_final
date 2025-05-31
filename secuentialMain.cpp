4db358bacd765c45671c4ec8932c83935d95a5a1
nahuel <nahuel.arrietamza@gmail.com>
2025-05-31 19:41:15 -0300

    fix secuential#

--- secuentialMain.cpp
+++ secuentialMain.cpp
@@ -33,7 +33,7 @@ int main(int argc, char** argv) {
         auto start = chrono::high_resolution_clock::now();
 
 
-        parallelQuickSort(row);
+        quickSort(row);
         
    
         // End timer
